d39936912497fc2251d0dd667cbd29f96a4603c1
Jai <jayapal1122@gmail.com>
2021-08-28 19:49:58 +0530

    Initial Commit

---
+++ src/main/java/com/jaiplays/services/payment/StripePaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jaiplays.services.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripePaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripePaymentGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaiplays/services/payment/StripePaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaiplays.services.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripePaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
